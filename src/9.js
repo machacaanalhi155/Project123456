48ddb60269cbbb220ec0bde00ed7f2dd0611bcbb
machacaanalhi155 <machacaanalhi155@gmail.com>
2025-03-14 23:29:03 +0000

    Add 9.js

---
+++ src/9.js
@@ -0,0 +1,42 @@
+
+  let Project123456 = {
+    "Universal History": {
+      "Early Stages": ["Cosmology", "The Big Bang", "Formation of the Universe"],
+      "Earth's Creation": ["Age of the Earth", "Formation of Planetary Systems", "Evolution of Life"],
+      "Human Exploration": ["Discovery of Ancient Civilizations", "Exploration of Mars", "Contact with Alien Life"]
+    },
+    "Planets": [
+      {
+        "Name": "Mercury",
+        "Orbital Data": ["Orbit around the Sun", "Closest to the Sun", "Smallest Planet"],
+        "Atmosphere": ["No atmosphere", "Hot and dry"]
+      },
+      {
+        "Name": "Venus",
+        "Orbital Data": ["Orbit around the Sun", "Closest to the Earth", "Similar in size to Earth"],
+        "Atmosphere": ["Thick atmosphere", "Toxic gases"]
+      },
+      {
+        "Name": "Mars",
+        "Orbital Data": ["Orbit around the Sun", "Closest to the Earth", "Barren landscape"],
+        "Atmosphere": ["Thin atmosphere", "Poor in oxygen"]
+      }
+    ],
+    "Stars": [
+      {
+        "Name": "Sun",
+        "Orbital Data": ["Orbit around center of galaxy", "Closest to Earth", "Brightest star in the sky"],
+        "Atmosphere": ["Gas giant planets", "No solid surface"]
+      },
+      {
+        "Name": "Earth",
+        "Orbital Data": ["Orbit around Sun", "Second closest to Sun", "Planetary system with liquid water and breathable atmosphere"],
+        "Atmosphere": ["Major cities", "Living organisms", "Cultural and technological advancements"]
+      },
+      {
+        "Name": "Neptune",
+        "Orbital Data": ["Orbit around Sun", "Farthest from Earth", "Eighth planet from Sun"],
+        "Atmosphere": ["No solid surface", "Ice and liquid water"]
+      }
+    ]
+  };
