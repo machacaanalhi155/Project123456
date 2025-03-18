182b128912ca129ebe9a57f951fced40972a1f86
machacaanalhi155 <machacaanalhi155@gmail.com>
2025-03-18 22:29:13 +0000

    Add 13.js

---
+++ src/13.js
@@ -0,0 +1,42 @@
+//Project123456 - A comprehensive study of the history of the universe.
+
+const universe = {
+  age: 13.8 billion years,
+  size: 93 million light-years in diameter,
+  expansionRate: 74 pixels per second,
+  galaxies: [
+    { name: "Andromeda", distance: 2.5 million light-years },
+    { name: "Milky Way", distance: 0 },
+    { name: "Tauri", distance: 1 billion light-years }
+  ],
+  blackHoles: [
+    { name: "Supermassive Black Hole at the center of Milky Way", mass: 2.6e+60 solar masses },
+    { name: "Black Hole at the center of Andromeda", mass: 3x10^5 solar masses }
+  ],
+  stars: [
+    { name: "Sun", mass: 1.989x10^30 grams, size: 12x10^6 kilometers },
+    { name: "Sirius", mass: 100 times larger than the sun, size: 50x10^6 kilometers }
+  ],
+  planets: [
+    { name: "Earth", orbit: 149.6 million kilometers from the Sun, size: 12,742 kilometers },
+    { name: "Mars", orbit: 227.9 million kilometers from the Sun, size: 6795 kilometers }
+  ],
+  moons: [
+    { name: "Moon", orbit: 384,000 kilometers around Earth, size: 12,104 kilometers },
+    { name: "Phobos", orbit: 67.9 million kilometers around Mars, size: 12 kilometer },
+    { name: "Deimos", orbit: 1.5 million kilometers around Mars, size: 2.27 kilometer }
+  ],
+  asteroids: [
+    { name: "4 Vesta", size: 100x10^6 meters, surfaceGravity: 1.3 times that of Earth },
+    { name: "Ceres", size: 550 miles, surfaceGravity: 0.26 times that of Earth }
+  ],
+  comets: [
+    { name: "Comet Hale-Bopp", orbit: 77.3 light-years from the Sun, diameter: 12 kilometers },
+    { name: "Comet ISON", orbit: 80 astronomical units from the Sun, diameter: 60 meters }
+  ],
+  dwarfPlanets: [
+    { name: "Pluto", size: 1.9 x 10^5 kilometers, mass: 0.2 times that of Earth },
+    { name: "Haumea", size: 3x10^6 kilometers, mass: 0.27 times that of Earth },
+    { name: "Makemake", size: 4x10^5 kilometers, mass: 0.5 times that of Earth }
+  ]
+};
